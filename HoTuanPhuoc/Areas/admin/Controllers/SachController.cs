18c04ce26f039f11b9e355555f8d25d55b75a02b
hophuoc4so9 <2224802010872@student.tdmu.edu.vn>
2024-10-21 22:45:14 +0700

    lab 6

---
+++ HoTuanPhuoc/Areas/admin/Controllers/SachController.cs
@@ -0,0 +1,189 @@
+﻿using HoTuanPhuoc.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace HoTuanPhuoc.Areas.admin.Controllers
+{
+    public class SachController : Controller
+    {
+        // GET: admin/Sach
+        SachOnlineEntities db = new SachOnlineEntities();
+        public ActionResult Index(int? page)
+        {
+            int iSize = 7;
+            int iPageNumber = page ?? 1;
+
+            List<SACH> kq = db.SACHes.OrderBy(a => a.MaSach).ToList();
+            return View(kq.ToPagedList(iPageNumber, iSize));
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(a => a.TenChuDe), "MaCD", "TenChuDe");
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(a => a.TenNXB), "MaNXB", "TenNXB");
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(FormCollection f, SACH sach, HttpPostedFileBase fFileUpload)
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(a => a.TenChuDe), "MaCD", "TenChuDe");
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(a => a.TenNXB), "MaNXB", "TenNXB");
+            if (fFileUpload == null)
+            {
+                //Nội dung thông báo yêu cầu chọn ảnh bìa
+                ViewBag.ThongBao = "Hãy chọn ảnh bìa.";
+                //Lưu thông tin để khi load lại trang do yêu cầu chọn ảnh bìa sẽ hiển thị   các thông tin này lên trang
+                ViewBag.TenSach = f["sTenSach"];
+                ViewBag.MoTa = f["sMoTa"];
+                ViewBag.SoLuong = int.Parse(f["iSoLuong"]);
+                ViewBag.GiaBan = decimal.Parse(f["mGiaBan"]);
+                ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe", int.Parse(f["MaCD"]));
+                ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB", int.Parse(f["MaNXB"]));
+                return View();
+            }
+            else
+            {
+
+                if (ModelState.IsValid)
+                {
+                    //Lấy tên file (Khai báo thư viện: System.IO)
+                    var sFileName = Path.GetFileName(fFileUpload.FileName);
+                    //Lấy đường dẫn lưu file
+                    var path = Path.Combine(Server.MapPath("~/Images"), sFileName); //Kiểm tra ảnh bìa đã tồn tại chưa để lưu lên thư mục
+                    if (!System.IO.File.Exists(path))
+                    {
+                        fFileUpload.SaveAs(path);
+
+                    }
+                    //Lưu Sạch vào CSDL
+                    sach.TenSach = f["sTenSach"];
+                    sach.MoTa = f["sMoTa"];
+                    sach.AnhBia = sFileName;
+                    sach.NgayCapNhat = Convert.ToDateTime(f["dNgayCapNhat"]);
+                    sach.SoLuongBan = int.Parse(f["iSoLuong"]);
+                    sach.GiaBan = decimal.Parse(f["mGiaBan"]);
+                    sach.MaCD = int.Parse(f["MaCD"]);
+                    sach.MaNXB = int.Parse(f["MaNXB"]);
+                    db.SACHes.Add(sach);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+                return View();
+            }
+        }
+        public ActionResult Details(int id)
+        {
+            SACH sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sach);
+        }
+        public ActionResult Delete(int id)
+        {
+            SACH sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sach);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id, FormCollection f)
+        {
+            var sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            var ctdh = db.CHITIETDATHANGs.Where(ct => ct.MaSach == id);
+            if (ctdh.Count() > 0)
+            {
+                ViewBag.ThongBao = "Sách này đang có trong bảng Chi tiết đặt hàng <br> Nếu muốn xóa thì phải xóa hết mã sách này trong bảng Chi tiết đặt hàng";
+                return View(sach);
+            }
+
+            var vietsach = db.VIETSACHes.Where(vs => vs.MaSach == id).ToList();
+            if (vietsach != null)
+            {
+                db.VIETSACHes.RemoveRange(vietsach);
+
+                db.SaveChanges();
+            }
+
+
+            db.SACHes.Remove(sach);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var sach = db.SACHes.SingleOrDefault(n => n.MaSach == id);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe", sach.MaCD);
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB", sach.MaNXB);
+
+            return View(sach);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(FormCollection f, HttpPostedFileBase fFileUpload)
+        {
+            int masach = int.Parse(f["MaSach"]);
+            var sach = db.SACHes.SingleOrDefault(n => n.MaSach == masach); 
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe),"MaCD", "TenChuDe", sach.MaCD);
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n =>n.TenNXB), "MaNXB", "TenNXB", sach.MaNXB);
+            if (ModelState.IsValid)
+            {
+                if (fFileUpload != null)
+                    {
+                    var sFileName = Path.GetFileName(fFileUpload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images"), sFileName); //Kiểm tra file đã tồn tại chưa
+
+                    if (!System.IO.File.Exists(path))
+                    {
+                        fFileUpload.SaveAs(path);
+                    }
+                    sach.AnhBia = sFileName;
+                }
+ 
+            sach.TenSach = f["TenSach"];
+                sach.MoTa = f["MoTa"];
+            sach.NgayCapNhat = Convert.ToDateTime(f["dNgayCapNhat"]);
+                sach.SoLuongBan = int.Parse(f["iSoLuong"]);
+                sach.GiaBan = decimal.Parse(f["mGiaBan"]);
+                sach.MaCD = int.Parse(f["MaCD"]);
+                sach.MaNXB = int.Parse(f["MaNXB"]);
+                db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(sach);
+        }
+
+ 
+    }
+}
